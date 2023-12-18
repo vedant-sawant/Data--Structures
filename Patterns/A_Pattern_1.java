22a13d95512fff5284b28854ad5102c58b50b795
vedant-sawant <vedantsawant208@gmail.com>
2023-12-18 23:31:11 +0530

    added Patterns 1

---
+++ Patterns/A_Pattern_1.java
@@ -0,0 +1,33 @@
+/*
+    Title: Pattern Basics - 1
+    Topic: Patterns [Loops]
+    Input: 3
+    Output: 
+		***
+		***
+		***
+    Author: Vedant Sawant
+    Date: 18/12/2023
+ */
+
+public class A_Pattern_1
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=3;j++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+
+	}
+}
+
+/*    OUTPUT
+123
+Reverse of the number is:
+321 
+*/
