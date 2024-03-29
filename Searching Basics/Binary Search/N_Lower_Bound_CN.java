c5328104709ed5e1d64dc85618789e01193d5769
vedant-sawant <vedantsawant208@gmail.com>
2024-03-29 20:09:55 +0530

    added upper and lower bound

--- Searching Basics/Binary Search/N_Lower_Bound_CN.java
+++ Searching Basics/Binary Search/N_Lower_Bound_CN.java
@@ -5,63 +5,23 @@
     Input: [1,2,2,3,3,5] & x=4
     Output: 5
     Author: Vedant Sawant
-    Date: 26/02/2024
+    Date: 29/03/2024
 */
-public class M_Search_In_Rotated_Sorted_33L
+
+class N_Lower_Bound_CN
 {
-    public static void main(String[] args) 
+    public static int lower_bound(int x,int arr[])
     {
-       int arr[]={4,5,6,7,0,1,2};
-       int target=5;
-       int pivot=pivotfind(arr);
-       System.out.println("Pivot Element Found at: "+pivot);
-       int first=binarysearch(arr,target,0,pivot);
-       if(first!=-1)
-       {
-            System.out.println("Target Found at Location: "+first);
-       }
-       else
-       {
-            System.out.println("Target Found at Location: "+binarysearch(arr,target,pivot+1,arr.length-1));
-       }
-    }
-    public static int pivotfind(int arr[])
-    {
+        int ans=arr.length;
         int low=0;
         int high=arr.length-1;
+        int mid=0;
         while(low<=high)
         {
-            int mid=(low+high)/2;
-            if(mid<high && arr[mid]>arr[mid+1])
+            mid=(low+high)/2;
+            if(arr[mid]>=x)
             {
-                return mid;
-            }
-            else if(mid>low && arr[mid]<arr[mid-1])
-            {
-                return arr[mid]-1;
-            }
-            else if(arr[low]>=arr[mid])
-            {
-                high=mid-1;
-            }
-            else if(arr[low]<arr[mid])
-            {
-                low=mid+1;
-            }
-        }
-        return -1;
-    }
-    public static int binarysearch(int arr[],int target,int low,int high)
-    {
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(arr[mid]==target)
-            {
-                return mid;
-            }
-            else if(arr[mid]>target)
-            {
+                ans=mid;
                 high=mid-1;
             }
             else
@@ -69,6 +29,21 @@ public class M_Search_In_Rotated_Sorted_33L
                 low=mid+1;
             }
         }
-        return -1;
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,1,2,3,4};
+        int x=1;
+        System.out.println(lower_bound(x,arr));
+        
     }
 }
+
+/*
+Input: 1 2 2 3
+       2
+       
+Ouput: 1 //index of the element 2
+ 
+ 
+ */
---
+++ Searching Basics/Binary Search/O_Upper_Bound_CN.java
@@ -0,0 +1,39 @@
+/*
+    Title: Coding Ninjas - Implement Upper Bound
+    PL: https://www.naukri.com/code360/problems/implement-upper-bound_8165383
+    Topic: Binary Search
+    Input: [1,2,2,3,3,5] & x=4
+    Output: 5
+    Author: Vedant Sawant
+    Date: 29/03/2024
+*/
+
+class O_Upper_Bound_CN
+{
+    public static int upper_bound(int x,int arr[])
+    {
+        int ans=arr.length;
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]>x)
+            {
+                ans=arr[mid];
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4};
+        int x=3;
+        System.out.println(upper_bound(x,arr));
+    }
+}
+
