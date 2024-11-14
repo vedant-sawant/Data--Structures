9ebac349649d644b106b9bf0d88c08f97c12e7f1
vedant-sawant <vedantsawant208@gmail.com>
2024-11-14 22:32:18 +0530

    added opps

---
+++ OOP Interview Prep/ARoom.java
@@ -0,0 +1,15 @@
+
+public class Room
+{
+    
+}
+public class ARoom 
+{
+    
+
+    public static void main(String[] args) 
+    {
+
+        
+    }
+}
---
+++ OOP Interview Prep/Room.java
@@ -0,0 +1,36 @@
+/*
+Imagine a Room class representing a real-world room. 
+The class should include properties like the room's dimensions (length, width, height) 
+and methods for calculating the area and volume of the room. Write a complete Java program 
+using this class with appropriate OOP concepts.
+*/
+class room
+{
+    private int length;
+    private int width;
+    private int height;
+    public void setroom(int l,int w,int h)
+    {
+        this.length=l;
+        this.height=h;
+        this.width=w;
+    }
+    public int calarea()
+    {
+        return length*height*width;
+    }
+
+    public int calcvolume()
+    {
+        return length+height+width;
+    }
+}
+public class Room {
+    public static void main(String[] args) 
+    {
+        room r=new room();
+        r.setroom(12, 14, 15);
+        System.out.println(r.calarea());
+        System.out.println(r.calcvolume());
+    }
+}
